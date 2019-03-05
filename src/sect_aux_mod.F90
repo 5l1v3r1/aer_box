4a7b3ce69376c185ec8b9f5caced076971762957
Sebastian D. Eastham <seastham@mit.edu>
2019-03-05 16:57:33 -0500

    Change data write to be comma-delimited
    Signed-off-by: Sebastian D. Eastham <seastham@mit.edu>

--- src/sect_aux_mod.F90
+++ src/sect_aux_mod.F90
@@ -29,10 +29,10 @@ module sect_aux_mod
   ! Write out the header line
   if (present(write_header)) then
     if (write_header) then
-      write(out_id,'(7(a16,x))',advance='no') 'Time (s)','Box','Temp (K)',&
+      write(out_id,'(a16,6(",",a16))',advance='no') 'Time (s)','Box','Temp (K)',&
              'Pres (hPa)','# (molec/cm3)','H2O (v/v)','H2SO4 gas (v/v)'
       do k=1,n_bins
-        write(out_id,'(a4,I3,a6,4x)',advance='no') 'Bin ',k,' (v/v)'
+        write(out_id,'(",",a4,I3,a6,3x)',advance='no') 'Bin ',k,' (v/v)'
       end do
       ! Advance to new line
       write(out_id,*)
@@ -41,11 +41,11 @@ module sect_aux_mod
 
   if (present(write_data).and.write_data) then
     ! Better hope all the necessary data is present
-    write(out_id,'(2(I16,x),3(E16.5E4,x))',advance='no') &
+    write(out_id,'(I16,",",I16,5(",",E16.5E4))',advance='no') &
         t_now, box_id, T_K, p_hPa, ndens, vvH2O, vvH2SO4
     ! VMR of each aerosol bin
     do k=1,n_bins
-      write(out_id,'(E16.5E4,x)',advance='no') vvSO4(k)
+      write(out_id,'(",",E16.5E4)',advance='no') vvSO4(k)
     end do
     ! Advance to new line
     write(out_id,*)
